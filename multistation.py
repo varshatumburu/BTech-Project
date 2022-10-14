f796c51a23158587739fb33fec451cfcc61d6400
Varsha <varshatumburu@gmail.com>
2022-10-14 10:09:36 +0530

    initial commit

---
+++ multistation.py
@@ -0,0 +1,37 @@
+import json
+import sched
+from scheduler import scheduling
+
+stations = json.load(open('charging_stations.json'))
+requests = json.load(open('requests.json'))
+
+def findNearestStation(coordinates):
+	mn = 1e9; x1=coordinates[0]; y1=coordinates[1]
+	idx=0
+	for c in stations:
+		if((x1-c['location'][0])**2 + (y1-c['location'][1])**2<mn):
+			mn = (x1-c['location'][0])**2 + (y1-c['location'][1])**2
+			idx = c['index']
+	return idx 
+
+reqMapping = dict()
+for i,r in enumerate(requests):
+	st = findNearestStation(r['location'])
+	if reqMapping.get(st)==None or len(reqMapping[st])==0:
+		reqMapping[st]=[]
+
+	reqMapping[st].append(i)
+
+leftover=[]
+for st in stations:
+	print(f"\nStation {st['index']}:")
+	# print(reqMapping[id])
+	reqidx = reqMapping[st['index']]
+	reqidx.extend(leftover)
+	print(reqidx)
+	req = [requests[i] for i in reqidx]
+	indices = scheduling(req)
+	leftover = list(set(reqidx)-set(indices))
+	print(leftover)
+
+
---
+++ scheduler.py
@@ -0,0 +1,146 @@
+# weighted job scheduling
+import json 
+from functools import total_ordering
+import datetime
+import math
+
+SLOT_TIME = 10
+# requests = json.load(open('requests.json'))
+# stations = json.load(open('charging_stations.json'))
+
+@total_ordering
+class Job:
+	def __init__(self, index, start, end, duration):
+		self.start = start
+		self.end = end
+		self.index = index
+		self.duration = duration
+
+	def __repr__(self) -> str:
+		return "{"+str(self.index)+","+str(self.start)+"-"+str(self.end)+"}"
+
+	def __lt__(self, other: object) -> bool:
+		return self.end< other.end
+
+
+def optimalNonConflict(arr, i, vis , sorted_list):
+	# for j in range(i-1, -1, -1):
+	# 	if(arr[j].end <= arr[i].start and (arr[i].index not in vis[j])):
+	# 		return j
+	for j in sorted_list:
+		if(arr[j[2]].end <= arr[i].start and (arr[i].index not in vis[j[2]])):
+			return j[2]
+		
+	return -1
+
+def optimize(jobs):
+	n=len(jobs)
+	# print(jobs)
+
+	total_time = [0] * n; global_dp = [0] * n
+	total_time[0]=jobs[0].duration 
+	global_dp[0]=total_time[0]
+
+	vis = {x:set() for x in range(0,n)}
+	gdp_vis = {x:set() for x in range(0,n)}
+	used = {y:[] for y in range(0,n)}
+
+	vis[0].add(jobs[0].index)
+	gdp_vis[0].add(jobs[0].index)
+	used[0].append(0)
+
+	sl = list()
+	sl.append(tuple([global_dp[0],1,0]))
+
+	idx=0
+	for i in range(1, n):
+
+		dur = jobs[i].duration 
+		l = optimalNonConflict(jobs, i, vis, sl)
+
+		# Add to duration if non-overlapping slot is found 
+		if l!=-1:
+			if jobs[i].index in vis[l]: 
+				continue
+			dur += total_time[l]
+			vis[i]=vis[l].copy()
+			used[i]=used[l].copy()
+
+		# print(i,dur)
+
+		total_time[i]=dur
+		vis[i].add(jobs[i].index)
+		used[i].append(i)
+
+		if(total_time[i]>global_dp[i-1] or (total_time[i]==global_dp[i-1] and len(vis[i])>len(gdp_vis[i-1]))):
+			global_dp[i]=total_time[i]
+			gdp_vis[i]=vis[i]
+			idx=i
+		else:
+			global_dp[i]=global_dp[i-1]
+			gdp_vis[i]=gdp_vis[i-1]
+
+		sl.append(tuple([global_dp[i], len(vis[i]), i]))
+		sl.sort(reverse=True)
+
+	return global_dp[n-1],used[idx]
+
+
+def scheduling(requests):
+
+	permuted_jobs = []
+	for r in requests:
+		interval = r['duration']
+		s = math.ceil(r['start_time']/SLOT_TIME) * SLOT_TIME
+
+		for start in range(s, r['end_time'], SLOT_TIME):
+			if(start+interval > r['end_time']): break
+			permuted_jobs.append(Job(r['index'], start, start+interval, interval))
+
+	permuted_jobs=sorted(permuted_jobs)
+	optimized_time, used_intervals = optimize(permuted_jobs)
+
+	selected_jobs = dict()
+	time_mapping = dict()
+	print("Slot Time =", SLOT_TIME, "mins")
+	for interval in used_intervals:
+		selected_jobs[permuted_jobs[interval].index]=permuted_jobs[interval]
+		for i in range(permuted_jobs[interval].start, permuted_jobs[interval].start + permuted_jobs[interval].duration, SLOT_TIME):
+			time_mapping[i]=permuted_jobs[interval].index
+		time = datetime.time(permuted_jobs[interval].start//60, permuted_jobs[interval].start%60)
+		print(f"Request {permuted_jobs[interval].index} scheduled at {time} for {permuted_jobs[interval].duration} mins.")
+
+	scheduled_jobs = [permuted_jobs[interval].index for interval  in used_intervals]
+	return scheduled_jobs
+
+	# # Take dynamic inputs 
+	# ind = len(requests)
+	# while input("Enter next request: (0 to break)")!="0":
+	# 	duration = int(input("Enter time required for charging:"))
+	# 	start = int(input("Enter start time of availability:"))
+	# 	end = int(input("Enter end of availability:"))
+
+	# 	while start+duration<=end:
+	# 		jb = Job(ind, start, start+duration, duration)
+	# 		print(time_mapping)
+	# 		if start in time_mapping.keys(): 
+	# 			occ = permuted_jobs[time_mapping[start]]
+	# 			newEndTime = start+duration+math.ceil(occ.duration/SLOT_TIME)*SLOT_TIME+SLOT_TIME
+	# 			fl=0
+	# 			while newEndTime <= requests[time_mapping[start]]['end_time']:
+	# 				if(time_mapping.get(newEndTime)==None):
+	# 					fl=1
+	# 					break
+	# 				occ.start += SLOT_TIME
+	# 				newEndTime += SLOT_TIME
+
+				
+
+					
+	# 		start+=SLOT_TIME
+			
+
+
+		
+
+
